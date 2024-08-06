df43756b3e8e76fb15b093a66f1f11aa190f6e9c
Veentikon <radzividlo.vitalii@gmail.com>
2024-08-06 13:21:19 -0600

    created the main window, starting point for this application

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 todo-app-python3
+
+GUI library: PyQt6
---
+++ main_screen.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("todo-app-vee")
+
+        button = QPushButton("my todo")
+        button.pressed.connect(self.close)
+
+        self.setCentralWidget(button)
+        self.show()
+
+app = QApplication(sys.argv)
+w = MainWindow()
+app.exec()
